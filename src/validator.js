5e4e518456f2851818f2cd44d460b0c91717e1fe
scarletsky <scarletsky1025@gmail.com>
2014-03-10 14:45:37 +0800

    feat: parse data messages and error

--- src/validator.js
+++ src/validator.js
@@ -5,9 +5,11 @@
             validators: function (fieldData) {
                 return fieldData.validators.split(' ');
             },
+
+            // return params where checker will used
             params: function (fieldData) {
                 var params = {};
-                var paramsList = [
+                var paramsMap = [
                     'length',
                     'lengthMin',
                     'lengthMax',
@@ -15,12 +17,54 @@
                     'rangeMax'
                 ];
 
-                $.each(paramsList, function (i, key) {
+                $.each(paramsMap, function (i, key) {
                     params[key] = fieldData[key];
                 });
 
                 return params;
             },
+
+            // return messages where the input tips will used
+            messages: function (fieldData) {
+                var msg = {};
+                var msgMap = [
+                    'msgRequire',
+                    'msgLength',
+                    'msgLengthMin',
+                    'msgLengthMax',
+                    'msgRange',
+                ];
+
+                $.each(msgMap, function (i, key) {
+                    var keyAlias = key.substring(3).replace(/\b\w+/g, function (word) {
+                        return word.substring(0, 1).toLowerCase() + word.substring(1);
+                    });
+
+                    msg[keyAlias] = fieldData[key];
+                });
+
+                return msg;
+            },
+            error: function (fieldData) {
+                var err = {};
+                var errMap = [
+                    'errRequire',
+                    'errLength',
+                    'errLengthMin',
+                    'errLengthMax',
+                    'errRange',
+                ];
+
+                $.each(errMap, function (i, key) {
+                    var keyAlias = key.substring(3).replace(/\b\w+/g, function (word) {
+                        return word.substring(0, 1).toLowerCase() + word.substring(1);
+                    });
+
+                    err[keyAlias] = fieldData[key];
+                });
+
+                return err;
+            }
         };
     })();
 
@@ -36,14 +80,16 @@
             // set the options object
             options.validators = parser.validators(fieldData);
             options.params = parser.params(fieldData);
+            options.messages = parser.messages(fieldData);
+            options.error = parser.error(fieldData);
 
             // init validator
             var validator = new $.validator(options);
 
+
             // validate
             $.each(validator.settings.validators, function (i, value) {
                 var result = validator.checker[value].call(this, fieldValue, validator.settings.params);
-                console.log(result);
             });
         }
     });
