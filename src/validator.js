f4d5e767d5572595336833a052e7cdf8c518a463
scarletsky <scarletsky1025@gmail.com>
2014-03-10 12:01:32 +0800

    feat: add data parser

--- src/validator.js
+++ src/validator.js
@@ -1,27 +1,38 @@
 ;(function ($) {
-    var utils = {
-        validators: function (validators) {
-            return validators.split(' ');
-        }
-    };
+    // parse data-*
+    var parser = (function () {
+        return {
+            validators: function (fieldData) {
+                return fieldData.validators.split(' ');
+            },
+            params: function (fieldData) {
+                var params = $.extend(true, {}, fieldData);
+                delete params.validators;
+                return params;
+            }
+        };
+    })();
 
     $.extend($.fn, {
         validate: function () {
-            var value = $(this).val();
-            var data = $(this).data();
+            // init private variables
+            var validateFlag = true;
             var options = {};
+            var field = $(this);
+            var fieldValue = field.val();
+            var fieldData = field.data();
 
-            $.each(data, function (key, val) {
-                if (utils[key]) {
-                    options[key] = utils[key](val);
-                }
-            });
+            // set the options object
+            options.validators = parser.validators(fieldData);
+            options.params = parser.params(fieldData);
 
-            // window just for test
-            window.validator = new $.validator(options);
+            // init validator
+            var validator = new $.validator(options);
 
-            $.each(validator.settings.validators, function (i, val) {
-                validator.checker[val](value);
+            // validate
+            $.each(validator.settings.validators, function (i, value) {
+                var result = validator.checker[value].call(this, fieldValue, validator.settings.params);
+                console.log(result);
             });
         }
     });
@@ -42,21 +53,53 @@
         prototype: {
             init: function () {},
             checker: {
-                require: function (value) {
-                    return $.trim(value).length > 0 ? true : false;
+                require: function (fieldValue) {
+                    return $.trim(fieldValue).length > 0;
                 },
-                length: function (value, length) {
-                    if (value.length !== length) {
-                        console.log('false');
+
+                length: function (fieldValue, params) {
+                    if (!params.length) {
+                        throw 'You should set `data-length` in the tag attr.';
                         return false;
                     }
-                    console.log('true');
-                    return true;
+                    return fieldValue.length === params.length;
                 },
-                range: function () {},
-                number: function () {},
-                telphone: function () {},
-                equalTo: function () {},
+
+                lengthMin: function (fieldValue, params) {
+                    if (!params.lengthMin) {
+                        throw 'You should set `data-length-min` in the tag attr';
+                        return false;
+                    }
+                    return fieldValue.length > params.lengthMin;
+                },
+
+                lengthMax: function (fieldValue, params) {
+                    if (!params.lengthMax) {
+                        throw 'You should set `data-length-max` in the tag attr';
+                        return false;
+                    }
+                    return fieldValue.length < params.lengthMax;
+                },
+
+                range: function (fieldValue, params) {
+                    if (!params.rangeMin && !params.rangeMax) {
+                        throw 'You should set `data-range-min` and `data-range-max` in the tag attr';
+                        return false;
+                    }
+
+                },
+
+                number: function (fieldValue) {
+
+                },
+
+                telphone: function (fieldValue) {
+
+                },
+
+                equalTo: function (fieldValue, params) {
+
+                },
             }
         }
     });
